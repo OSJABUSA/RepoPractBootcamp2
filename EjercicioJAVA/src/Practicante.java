20a4047bc55b24bb964d9d43df6af510d73ef166
montesm15 <montesm90@gmail.com>
2025-10-27 20:30:45 -0500

    feat: Desarrollo clase Practicante

---
+++ EjercicioJAVA/src/Practicante.java
@@ -0,0 +1,35 @@
+public class Practicante {
+    private String universidad;
+    private int horasPorSemana;
+
+     // Constructores sin parametros y con parametros
+
+     public Practicante(){
+        super();
+        this.universidad = "";
+        this.horasPorSemana = 0;
+    }
+
+     public Practicante(String universidad, int horasPorSemana) {
+        this.universidad = universidad;
+        this.horasPorSemana = horasPorSemana;
+     }
+
+     public String getUniversidad() {
+         return universidad;
+     }
+
+     public void setUniversidad(String universidad) {
+         this.universidad = universidad;
+     }
+
+     public int getHorasPorSemana() {
+         return horasPorSemana;
+     }
+
+     public void setHorasPorSemana(int horasPorSemana) {
+         this.horasPorSemana = horasPorSemana;
+     }
+
+    
+}
